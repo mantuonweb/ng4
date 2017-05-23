260ae6668af3f5ec0984aa7e1ba0698ba73ba219
mantuonweb <mantu.nigam@globallogic.com>
2017-05-23 21:02:49 +0530

    scss added
    scss added

--- src/app/Home/Contact/Default/ContactHome.ts
+++ src/app/Home/Contact/Default/ContactHome.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }           from '@angular/router';
 import { AuthService } from '../../../common/AuthService';
+import { ContactService } from '../../../common/ContactService';
  @Component({
   selector: 'mbs-contact',
   templateUrl: 'app/Home/Contact/Default/ContactHome.html',
   styleUrls: [ 'app/Home/Contact/Contact.css' ]
 })
 export class ContactHomeComponent implements OnInit {
-  username:string="";
-  password:string="";
   loading:boolean=false;
-  constructor(private router: Router, private auth:AuthService)
+  contacts:any[];
+  sub:any;
+  constructor(private router: Router, private auth:AuthService ,private contactService:ContactService)
   {
     console.log('LOGIN',auth);
   }
   ngOnInit() {
-
+    this.loadContacts();
   }
+  onContactChange(contact:any){
+    this.router.navigateByUrl('/home/'+contact.contactId);
+  }
+
+  loadContacts(){
+     // Get all contacts
+     this.contactService.getContacts().subscribe(
+     contacts => this.contacts = contacts, //Bind to view
+     err => {
+          console.log(err);
+     });
+    }
 }
