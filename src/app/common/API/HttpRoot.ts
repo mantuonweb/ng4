5d43edab488ff518bd240a5c82f9b1cab6ad659e
mantuonweb <mantu.nigam@globallogic.com>
2017-06-04 11:12:15 +0530

    some modification
    some mdification

--- src/app/common/API/HttpRoot.ts
+++ src/app/common/API/HttpRoot.ts
@@ -1,6 +1,6 @@
 //http://plnkr.co/edit/5gb5E4curAE2EfH2lNZQ?p=preview
 //https://thinkster.io/tutorials/angular-2-http
-https://stackoverflow.com/questions/44342082/rest-request-using-inheritance-in-angular-2
+//https://stackoverflow.com/questions/44342082/rest-request-using-inheritance-in-angular-2
 import { Injector }    from '@angular/core';
 import 'rxjs/add/operator/map';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
